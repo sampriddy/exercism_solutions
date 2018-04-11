24ae73847189928418f5e789ead08da638127058
Sam Priddy <priddy.samuel@gmail.com>
2018-04-10 23:04:11 -0400

    javascript reverse string

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -8,7 +8,7 @@ var Year = function (year) {
 };
 
 Year.prototype.isLeap = function () {
-  return (this.year % 400 == 0) || (this.year % 4 == 0 && this.year % 100 != 0) 
+  return (this.year % 400 == 0) || (this.year % 4 == 0 && this.year % 100 != 0)
 };
 
 module.exports = Year;
