674c5ba4bb86abdb7a7870c154494ddcdd4f6365
Sam Priddy <priddy.samuel@gmail.com>
2018-05-07 09:08:10 -0400

    Elixir rotational cipher solution 1
    Converts string input into charlists, maps, and converts back to string.
    Some deduplication leftover.

---
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -0,0 +1,27 @@
+defmodule RotationalCipher do
+  @lower ?a..?z
+  @upper ?A..?Z
+
+  @doc """
+  Given a plaintext and amount to shift by, return a rotated string.
+
+  Example:
+  iex> RotationalCipher.rotate("Attack at dawn", 13)
+  "Nggnpx ng qnja"
+  """
+  @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, shift) do
+    text
+    |> String.to_charlist
+    |> Enum.map(fn(char) -> rotate_char(char, shift) end)
+    |> Kernel.to_string
+  end
+
+  def rotate_char(char, shift) when char in @lower do
+    rem(char - ?a + shift, 26) + ?a
+  end  
+  def rotate_char(char, shift) when char in @upper do
+    rem(char - ?A + shift, 26) + ?A
+  end
+  def rotate_char(char, _), do: char
+end
